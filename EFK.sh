ade96babf8914cbc4779125aefed5f9ce8e23bf2
Viacheslav <you@example.com>
2019-02-13 01:32:01 +0200

    change volume size

--- EFK.sh
+++ EFK.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-kubectl create -f rbac-config.yml
-helm init --service-account tiller --upgrade
-helm install stable/elasticsearch --name elasticsearch --namespace logging --set client.heapSize=512m --set master.heapSize=512m --set data.replicas=1 --set master.replicas=2 --set client.replicas=1 --set data.heapSize=512m
+helm install stable/elasticsearch --name elasticsearch --namespace logging --set client.heapSize=512m --set master.heapSize=512m --set data.replicas=1 --set master.replicas=2 --set client.replicas=1 --set data.heapSize=512m --set master.persistence.size=2Gi --set data.persistence.size=5Gi
 helm install stable/fluentd-elasticsearch --name fluentd --namespace logging --set elasticsearch.host=elasticsearch-client
 helm install stable/kibana --set env.ELASTICSEARCH_URL=http://elasticsearch-client:9200 --name kibana --namespace logging
 
